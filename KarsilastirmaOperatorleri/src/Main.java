d8774efb31a9c025640be00be5f08ac7fcb51cde
paraglidingpilot <hakancinarel@gmail.com>
2021-09-26 20:04:02 +0300

    Java

---
+++ KarsilastirmaOperatorleri/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        Karşılaştırma Operatörleri 
+        
+            == --> Eşit mi?
+            != --> Eşit değil mi?
+            > ---> Büyük mü?
+            >= ---> Büyük veya Eşit mi?
+            < ---> Küçük mü ?
+            <= ---> Küçük veya Eşit mi?
+        */
+        System.out.println(3 == 4); 
+        System.out.println(3 != 4);
+        System.out.println(3 > 4);
+        System.out.println(3 >= 4);
+        System.out.println(3 < 4);
+        System.out.println(3 <= 4);
+        
+        System.out.println("Hakan" == "Hakan");
+        
+    }
+}
---
+++ KodBlokları/src/Main.java
@@ -0,0 +1,34 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        // a ve b main kod bloğu içinde.
+    }
+    int c;
+
+    // Kodları düzenli yazmak için;
+    /*
+    1-
+    int a = 5; Düzenli
+    int a                     =    5; Düzensiz
+    int b 
+    
+    =
+    5; düzensiz
+    
+    2-
+    
+    int a =5; double d = 5d; float  = 4.2f; // Düzensiz
+    int a = 5;
+    int b = 7;
+    int c = 8;
+    düzenli 
+    
+    3- Yorumlar önemli 
+    
+    
+    
+     */
+}
